46f0bc2af8176bf04c4b279c80ebb34838ee6dd3
188229624 <188229624@qq.com>
2022-04-12 12:03:16 +0800

    rem更改

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,13 +1,17 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-module.exports = {
-  plugins: {
-    autoprefixer: {
-      overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
-    },
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*'],
-      //selectorBlackList: ['van-']
-    }
+const path = require('path');
+module.exports = ({file})=>{
+  const designWidth = file.dirname.includes(path.join('node_modules', 'vant')) ? 37.5 : 75.0;
+  return {
+	  plugins: {
+	    autoprefixer: {
+	      overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+	    },
+	    'postcss-pxtorem': {
+	      rootValue: designWidth,
+	      propList: ['*'],
+	      //selectorBlackList: ['van-']
+	    }
+	  }
   }
 }
